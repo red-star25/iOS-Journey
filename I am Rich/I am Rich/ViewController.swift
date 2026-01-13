1406f43ba4e2617668b6ee301b6a39c6b9929317
Dhruv Nakum <nakumdhruv123@gmail.com>
2026-01-13 11:03:23 +0530

    I am rich

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Dhruv Nakum on 12/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I am Rich/I am RichTests/I_am_RichTests.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_RichTests.swift
+//  I am RichTests
+//
+//  Created by Dhruv Nakum on 12/26/25.
+//
+
+import Testing
+@testable import I_am_Rich
+
+struct I_am_RichTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
