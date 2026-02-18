63c42da3add6d2381bbb3f958f20e14befc15c46
Dhruv Nakum <nakumdhruv123@gmail.com>
2026-02-17 22:22:38 -0800

    Added image carousal in product detail

--- XMart/XMart/Domain/Models/Product.swift
+++ XMart/XMart/Domain/Models/Product.swift
@@ -11,11 +11,12 @@ struct Product: Identifiable, Decodable {
     let description: String
     let price: Double
     let thumbnail: String
+    let images: [String]
 }
 
 
 extension Product {
     static func sampleProduct() -> Product {
-        return Product(id: 1, title: "Test Title", description: "product description", price: 11.99, thumbnail: "https://greendroprecycling.com/wp-content/uploads/2017/04/GreenDrop_Station_Aluminum_Can_Coke.jpg")
+        return Product(id: 1, title: "Test Title", description: "product description", price: 11.99, thumbnail: "https://greendroprecycling.com/wp-content/uploads/2017/04/GreenDrop_Station_Aluminum_Can_Coke.jpg", images: ["https://greendroprecycling.com/wp-content/uploads/2017/04/GreenDrop_Station_Aluminum_Can_Coke.jpg","https://anchanto.com/wp-content/uploads/2024/11/e-commerce-product-image-best-practices-by-anchanto.jpg", "https://cdn.shopify.com/s/files/1/0070/7032/articles/ecommerce_20product_20recommendation_2f1656f9-4c76-427d-b91f-0f9730e1bdc0.png?v=1729516733"])
     }
 }
--- XMart/XMart/UI/Shop/ProductDetailView.swift
+++ XMart/XMart/UI/Shop/ProductDetailView.swift
@@ -9,41 +9,80 @@ import SwiftUI
 
 struct ProductDetailView: View {
     let product: Product
+    @State private var selectedImage: String?
     
     
     var body: some View {
         NavigationStack {
             ScrollView {
                 VStack(alignment: .leading, spacing: 12) {
-                    if let url = URL(string: product.thumbnail) {
-                        AsyncImage(url: url) { phase in
-                            switch phase {
-                            case .empty:
-                                ProgressView().frame(maxWidth: .infinity, minHeight: 220)
-                            case .success(let image):
-                                image
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(maxWidth: .infinity)
-                                    .clipShape(RoundedRectangle(cornerRadius: 16))
-                                    .padding(.horizontal)
-                            case .failure:
-                                Image(systemName: "photo")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 60,height: 60)
-                                    .foregroundStyle(.secondary)
-                                    .frame(maxWidth: .infinity, minHeight: 220)
-                            @unknown default:
-                                EmptyView()
+                    ZStack {
+                        if let url = URL(string: selectedImage ?? product.thumbnail) {
+                            AsyncImage(url: url) { phase in
+                                switch phase {
+                                case .empty:
+                                    ProgressView().frame(maxWidth: .infinity, minHeight: 220)
+                                case .success(let image):
+                                    image
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(maxWidth: .infinity, minHeight: 200)
+                                        .clipShape(RoundedRectangle(cornerRadius: 16))
+                                        .padding(.horizontal)
+                                case .failure:
+                                    Image(systemName: "photo")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 60,height: 60)
+                                        .foregroundStyle(.secondary)
+                                        .frame(maxWidth: .infinity, minHeight: 220)
+                                @unknown default:
+                                    EmptyView()
+                                    
+                                }
                                 
                             }
-                            
                         }
                     }
+                    .frame(height: 280)
+                    .frame(maxWidth: .infinity)
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                    .padding(.all)
+                    
                     Text(product.title)
                     Text("$\(product.price, specifier: "%.2f")")
                         .font(.title3).bold()
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 10) {
+                            ForEach(product.images, id:\.self) { urlString in
+                                if let url = URL(string: urlString) {
+                                    AsyncImage(url: url) { phase in
+                                        switch phase {
+                                        case .empty:
+                                            ProgressView().frame(maxWidth: 50, minHeight: 50)
+                                        case .success(let image):
+                                            image
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(maxWidth: 70, maxHeight: 70)
+                                        case .failure:
+                                            Image(systemName: "photo")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 60,height: 60)
+                                                .foregroundStyle(.secondary)
+                                                .frame(maxWidth: 50, minHeight: 50)
+                                        @unknown default:
+                                            EmptyView()
+                                        }
+                                    }
+                                    .onTapGesture {
+                                        selectedImage = urlString
+                                    }
+                                }
+                            }
+                        }
+                    }
                     Text(product.description)
                         .font(.body)
                         .foregroundColor(.secondary)
@@ -53,6 +92,11 @@ struct ProductDetailView: View {
             }
             .navigationTitle("Details")
             .navigationBarTitleDisplayMode(.inline)
+        }
+        .onAppear {
+            if selectedImage == nil {
+                selectedImage = product.thumbnail
+            }
         }
     }
 }
